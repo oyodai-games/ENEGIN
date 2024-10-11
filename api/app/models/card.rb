782f9f1f213a79e6f7756abf1d540bd9498cc309
dishizawa <dishizawa2001@gmail.com>
2024-10-12 01:09:31 +0900

    issue:10 :CardsテーブルとEnemiesテーブルのリレーションを作成

--- api/app/models/card.rb
+++ api/app/models/card.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# Cardモデルは、敵キャラクター（Enemy）に属している
+# 1つのカードは1体のEnemyに関連付けられる1対多のリレーション）
+# enemy_idを使って関連付けが行われる
 class Card < ApplicationRecord
+  belongs_to :enemy
 end
--- api/app/models/enemy.rb
+++ api/app/models/enemy.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# Enemyモデルは、複数のカード（Card）を持つ1対多のリレーションを持っている
+# 1体のEnemyが複数のCardを所有し、それぞれのCardはこのEnemyに属する
+# cardsテーブルにはenemy_idカラムがあり、このカラムを使ってEnemyと関連付けられる
 class Enemy < ApplicationRecord
+  has_many :card
 end
